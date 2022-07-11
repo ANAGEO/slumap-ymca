9e15019345a2a7ca142b46bc88ae2bf507eb3397
jgovoort <julien.govoorts@gmail.com>
2022-07-11 14:47:01 +0200

    modification de slumage. Code is running. Il divise bien le raster en fonction des polygones contenus dans le shapefile. Test réalisé avec s1_vv

--- slumage.py
+++ slumage.py
@@ -2,13 +2,41 @@ from osgeo import gdal
 from osgeo import ogr
 import os, sys
 import geopandas
+import json
 
-in_path = "/media/student/data/docker-jupyter/Images/S1/decoup/"
-in_path_vect = "/media/student/data/julien"
-input_filename = "S1_VV_SMALL_"
+def extract_coord(output_poly): #fonction pour extraire les coordonnées géographiques à partir d'un fichier json
+    
+    extract_json = json.loads(output_poly)
+    coordinates = extract_json['coordinates']
+    xmin = coordinates[0][0][0][0]
+    xmax = coordinates[0][0][1][0]
+    ymin = coordinates[0][0][0][1]
+    ymax = coordinates[0][0][2][1]
+    
+    return xmin,xmax,ymin,ymax
+    
 
-format_file = ".tif"
+#in_path = "/media/student/data1/docker-jupyter/Images/S1/"
+#in_path_vect = "/media/student/data1/julien/sample/"
+#raster_filename = "S1_VV_SMALL_"
 
-raster = gdal.Open(in_path+input_filename+str(1)+format_file) #importation de la matrice
+#vector_filename = "sample_small.gpkg"
 
+#format_file = ".tif"
 
+vector = ogr.Open('Images/smaller/sample/sample_grid.gpkg') #ouverture de la fichier gkpg
+layer = vector.GetLayer() #extraction des couches dans le gkpg dans ce cas-ci il n'y en a qu'une seule (sinon spécifier)
+print(layer.GetExtent()) #affiche de l'emprise de la couche vectorielle
+
+for feat in layer: #une boucle sur les polygones dans la couche vectorielle
+    polygone = feat.GetGeometryRef().ExportToJson() #extraire les données géométriques et exporter en format JSON
+    print(extract_coord(polygone)) #extraction des coordonnées des polygones
+    poly_id = feat.GetField("id") #extraire le id de l'object
+    lulc = feat.GetField("lulc") #extraire le lulc (si slum ou pas slum) de l'object
+
+    print(str(poly_id))
+    print(lulc)
+    
+#raster = gdal.Open(in_path+raster_filename+str(1)+format_file) #importation de la matrice
+
+#vect = "ogrinfo /media/student/data1/julien/smaller/sample/sample_small.gpkg -al -fid 1"
