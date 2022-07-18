dd533c7cfc3affa7067f92124cd7efb75a7abb3a
jgovoort <julien.govoorts@gmail.com>
2022-07-18 16:57:17 +0200

    Avancement sur le Y model et données SAR

--- chunckage.py
+++ chunckage.py
@@ -17,7 +17,7 @@ def extract_coord(output_poly): #fonction pour extraire les coordonnées géogra
     return xmin,ymin,xmax,ymax
 
 
-def chunckage(folder_raster,filename_raster,format_rast,folder_vector, filename_vector):
+def chunckage(folder_raster,filename_raster,format_rast,folder_vector, filename_vector, output_name, lulc_yes):
     
     rast = gdal.Open(folder_raster+filename_raster+format_rast) #ouverture du raster
 
@@ -27,7 +27,7 @@ def chunckage(folder_raster,filename_raster,format_rast,folder_vector, filename_
 
     print(layer.GetExtent()) #affiche de l'emprise de la couche vectorielle
    
-    os.system('mkdir ./'+folder_raster+filename_raster+'_patch') #création automatique de la directory pour enregistrer les patchs
+    os.system('mkdir ./'+folder_raster+output_name+'_patch') #création automatique de la directory pour enregistrer les patchs
     
     #étape d'initialisation de la boucle
     patch_id = [] #création des matrices
@@ -39,14 +39,23 @@ def chunckage(folder_raster,filename_raster,format_rast,folder_vector, filename_
             list_coord = extract_coord(polygone) #extraction des coordonnées des polygones
             print(list_coord)
             poly_id = int(feat.GetField("id")) #extraire le id de l'object
-            lulc = int(feat.GetField("lulc")) #extraire le lulc (si slum ou pas slum) de l'object
-            name_file= filename_raster+'_'+str(poly_id)+'_'+str(lulc)+'.tif' #création du nom du fichier de sortie
-            filename = folder_raster+filename_raster+'_patch/'+str(name_file)
+            print(poly_id)
+            
+            if lulc_yes == 1 : 
+                lulc = int(feat.GetField("lulc")) #extraire le lulc (si slum ou pas slum) de l'object
+                name_file= output_name+'_'+str(poly_id)+'_'+str(lulc)+'.tif' #création du nom du fichier de sortie
+                patch_lulc.append(lulc) #enregistrement dans une liste si le patch est un slum ou pas
+            elif lulc_yes == 0 :
+                name_file= output_name+'_'+str(poly_id)+'.tif' #création du nom du fichier de sortie
+            
+            filename = folder_raster+output_name+'_patch/'+str(name_file)
             gdal.Warp(str(filename),rast, outputBounds = list_coord) #division en petite tuile correspondante aux polygone dans le shpfile
             listvrt.append(filename) #enregistremet dans une liste le chemin des patchs
             
             patch_id.append(poly_id) #enregistrement dans une liste l'id du patch
-            patch_lulc.append(lulc) #enregistrement dans une liste si le patch est un slum ou pas
-
             
-    return listvrt, patch_id, patch_lulc
+
+    if lulc_yes == 1 :         
+        return listvrt, patch_id, patch_lulc
+    elif lulc_yes == 0 : 
+        return listvrt, patch_id
